09e8715f159920b17f53902994702cfb90605257
Shawn <shawntaylor@weecology.org>
2015-12-02 11:38:19 -0500

    this seems to work

---
+++ convertToTif.py
@@ -0,0 +1,15 @@
+import glob
+import os
+from subprocess import call
+
+dataDir='/home/shawn/data/modis/'
+outputDir='/home/shawn/data/modisReprojected/'
+mrtPath='/home/shawn/bin/bin/resample'
+resampleConfigFile='./resampleConfig.prm'
+
+hdfFiles = glob.glob(os.path.join(dataDir+'*hdf'))
+
+for thisFile in hdfFiles:
+    #Keeps the 1st 3 sections of the modis filename. Like MOD13A1-A2008144-h10v04
+    outputFile=outputDir+'-'.join(os.path.basename(thisFile).split('.')[0:3])+'.tif'
+    call([mrtPath, '-i', thisFile, '-o', outputFile, '-p', resampleConfigFile ])
